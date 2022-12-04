c2190834e8cc260a767030d1d1cd28cdf521c23f
DilshodM <dilshodmurtazoyev5@gmmail.com>
2022-12-04 23:32:34 +0500

    last day

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,94 +1,100 @@
 /**
-* Template Name: ComingSoon - v4.8.1
-* Template URL: https://bootstrapmade.com/comingsoon-free-html-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
+ * Template Name: ComingSoon - v4.8.1
+ * Template URL: https://bootstrapmade.com/comingsoon-free-html-bootstrap-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function () {
   "use strict";
 
   /**
    * Easy selector helper function
    */
   const select = (el, all = false) => {
-    el = el.trim()
+    el = el.trim();
     if (all) {
-      return [...document.querySelectorAll(el)]
+      return [...document.querySelectorAll(el)];
     } else {
-      return document.querySelector(el)
+      return document.querySelector(el);
     }
-  }
+  };
 
   /**
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
+    let selectEl = select(el, all);
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach((e) => e.addEventListener(type, listener));
       } else {
-        selectEl.addEventListener(type, listener)
+        selectEl.addEventListener(type, listener);
       }
     }
-  }
+  };
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+    el.addEventListener("scroll", listener);
+  };
 
   /**
    * Back to top button
    */
-  let backtotop = select('.back-to-top')
+  let backtotop = select(".back-to-top");
   if (backtotop) {
     const toggleBacktotop = () => {
       if (window.scrollY > 100) {
-        backtotop.classList.add('active')
+        backtotop.classList.add("active");
       } else {
-        backtotop.classList.remove('active')
+        backtotop.classList.remove("active");
       }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
   }
 
   /**
    * Countdown timer
    */
-  let countdown = select('.countdown');
+  let countdown = select(".countdown");
   const output = countdown.innerHTML;
 
-  const countDownDate = function() {
-    let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
+  const countDownDate = function () {
+    let timeleft =
+      new Date(countdown.getAttribute("data-count")).getTime() -
+      new Date().getTime();
 
     let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let hours = Math.floor(
+      (timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-    countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
-  }
+    countdown.innerHTML = output
+      .replace("%d", days)
+      .replace("%h", hours)
+      .replace("%m", minutes)
+      .replace("%s", seconds);
+  };
   countDownDate();
   setInterval(countDownDate, 1000);
+})();
 
-})()
+let name = document.getElementById("");
+let button = document.getElementById("sendTelegram");
 
-let name = document.getElementById('')
-let button = document.getElementById('sendTelegram')
-
-
-
-
-let a = ()=> axios.post('https://api.telegram.org/bot5626053499:AAF5nY9wWWTUoUmmLoeflMvCtUvlaayI7vU/sendmessage?chat_id=950348637&text=test', 'sd')
-
-button.addEventListener('click', ()=>{
-  a()
-})
-
+let a = () =>
+  axios.post(
+    "https://api.telegram.org/bot5626053499:AAF5nY9wWWTUoUmmLoeflMvCtUvlaayI7vU/sendmessage?chat_id=950348637&text=test",
+    "sd"
+  );
+button.addEventListener("click", () => {
+  a();
+});
 
 // -1001642583555
 
